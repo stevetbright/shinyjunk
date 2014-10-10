28bd46b79ac236d7c8d5e3a388b6163e5b2f3c3c
stevetbright <stevetbright@gmail.com>
2014-10-09 18:48:28 -0700

    demo app

---
+++ packages_to_install.R
@@ -0,0 +1,14 @@
+
+
+devtools::install_github('rstudio/shinyapps')
+
+install.packages('ggvis')
+install.packages('ggplot2')
+install.packages('shiny')
+install.packages('dplyr')
+install.packages('tidyr')
+install.packages("rtools")
+
+shinyapps::setAccountInfo(name='stevetbright', token='D6BCDC14D7FEAAB91351F7A1596EB284', secret='JImDlPvBeS05ErrN0gtfoD0B176WhOqCMygXTX9E')
+
+
